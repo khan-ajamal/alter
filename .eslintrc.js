76dd826a78b69620e302a06ce78e6dc2118008f4
Ajamal Khan <ajamalkhan65@gmail.com>
2020-10-26 17:30:46 +0530

    Add nodemon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   env: {
+    commonjs: true,
     es2021: true,
     node: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
   },
-  rules: { 'prettier/prettier': 'error' },
+  rules: {
+    'prettier/prettier': ['error'],
+  },
 };
